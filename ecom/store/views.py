7169b737d0c27c6ebc8126d4cc2c030f99c83e99
Arslan8869 <muhammadadnan8869@gmail.com>
2026-03-20 15:56:06 +0500

    add category filter page

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
+
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'category': category, 'products': products})
+    except Category.DoesNotExist:
+        messages.error(request, 'Category does not exist.')
+        return redirect('home')
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
+
 
 def home(request):
     products = Product.objects.all()
@@ -29,3 +50,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out.')
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Unsuccessful registration. Invalid information.')
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form})
