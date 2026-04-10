0a4ab85503ed7ef4a429099debb149160eab256f
Arslan8869 <muhammadadnan8869@gmail.com>
2026-04-10 10:53:22 +0500

    fix(media): serve media files in production when DEBUG is false

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import settings
+from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +11,13 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls')),
     path('accounts/', include('allauth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve,
+                {'document_root': settings.MEDIA_ROOT}),
+    ]
